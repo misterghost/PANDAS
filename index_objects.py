281fb8b51eca01330c43e7819f60d30c41ea21ac
misterghost <arquitectolizcano@gmail.com>
2016-11-10 12:36:01 -0600

    reindexing problems

--- index_objects.py
+++ index_objects.py
@@ -10,13 +10,79 @@ from pandas import Series, DataFrame
 import pandas as pd
 
 
-#Starting with a dict of equal length lists:
-data = {
-        'State':['ohio', 'nevada', 'oklahoma', 'texas', 'louisiana'], 
-        'Year':[2000, 2001, 1999, 2003, 2005],
-        'Population': [1.5, 1.3, 1.6, 1.2, 2.0]
-        }
+#Starting with an array:
+data = ['ohio', 'nevada', 'oklahoma', 'texas', 'louisiana']
         
-obj = Series(range(3), index=['a', 'b', 'c'])
-        
-index = obj.index
+#Convert array called data to a Series.
+#Series is a fixed length, ordered dictionary.
+object = Series(data)
+print(object)  
+#Specify index.
+object_with_index = Series(data, index=['first state', 'second state', 
+                                        'third state', 'fourth state', 
+                                        'fifth state'])
+print(object_with_index)       
+
+#Index OBJECTS
+#Holds labels or metadata like axis name.
+#Any array or sequence can be converted to an Index.
+
+#Starting with an array:
+data2 = ['ohio', 'nevada', 'oklahoma', 'texas', 'louisiana']
+
+#Convert to series and add indexes.
+object_with_index2 = Series(data2, index = ['1st State', '2nd state',
+                                            '3rd State', '4th State',
+                                            '5th State'])
+print(object_with_index2)
+
+#Make those indexes, indexes objects:
+index = object_with_index2.index
+
+#Get one of those indexes:
+print('Index number one is: ', index[:1])
+
+#Index Objects are inmutable:
+print('Inmutable Index Objects:')
+print(index[:1])
+#Change it (uncomment next line):
+#index[1] = '2rd State'
+#Error.
+
+
+#Indexes are being array-like. You can search among them.
+print("Let's search for an index in all the indexes:")
+if '1st State' in object_with_index2:
+    print('List of indexes contains "1st State". ')
+else:
+    print("No it doesn't.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
